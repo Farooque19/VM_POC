60282e18fec5d6497f831782bee8c24831fcc077
Farooque Ahmed <fa6578718@gmail.com>
2025-01-09 18:42:33 +0530

    new commit

--- app/routes/vm.$id.tsx
+++ app/routes/vm.$id.tsx
@@ -1,24 +1,24 @@
-import { useFetcher, useLoaderData } from '@remix-run/react';
-import { AppProvider, Page, Card, MediaCard } from '@shopify/polaris';
-import type { ActionFunction, LoaderFunctionArgs, ActionFunctionArgs } from "@remix-run/node";
-import { authenticate } from "../shopify.server";
-import { useState } from 'react';
+import {useFetcher, useLoaderData} from '@remix-run/react';
+import {AppProvider, Page, Card, MediaCard} from '@shopify/polaris';
+import type {LoaderFunctionArgs, ActionFunctionArgs} from "@remix-run/node";
+import {authenticate} from "../shopify.server";
+import {useState} from 'react';
 
 // interface ReorderData {
 //   id: string;
 //   moves: { id: string; newPosition: string }[];
 // }
 
-export const loader = async ({ request, params }: LoaderFunctionArgs) => {
-  const { id } = params;
-  const { admin } = await authenticate.admin(request);
+export const loader = async ({request, params}: LoaderFunctionArgs) => {
+  const {id} = params;
+  const {admin} = await authenticate.admin(request);
   let hasNextPage: boolean = true;
   let afterCursor: string | null = null;
   let products: any[] = [];
   let ids: string[] = [];
 
   let collectionId: string = `gid://shopify/Collection/${id}`;
-  while(hasNextPage) {
+  while (hasNextPage) {
     const response = await admin.graphql(`
       query GetNextCollectionProducts($id: ID!, $after: String) {
         collection(id: $id) {
@@ -30,6 +30,23 @@ export const loader = async ({ request, params }: LoaderFunctionArgs) => {
                 title
                 handle
                 vendor
+                productType
+                tags
+                featuredMedia {
+                  preview {
+                    image {
+                      altText
+                      url
+                    }
+                  }
+                }
+                createdAt
+                updatedAt
+                publishedAt
+                totalInventory
+                variantsCount {
+                  count
+                }
               }
             }
             pageInfo {
@@ -40,38 +57,73 @@ export const loader = async ({ request, params }: LoaderFunctionArgs) => {
         }
       }
     `,
-    {
-      variables: {
-        id: collectionId,
-        after: afterCursor
+      {
+        variables: {
+          id: collectionId,
+          after: afterCursor
+        }
       }
-    }
     );
     const productsData: any = (await response.json()).data;
-    
+
     products.push(...productsData.collection.products.edges);
-    for(let product of products){
+    for (let product of products) {
       ids.push(product.node.legacyResourceId);
     }
-    console.log('Sequence id:');
-    for(let id of ids){
-      console.log(id+"\n");
+    let hasNextVariantPage: boolean = true;
+    let afterVariantCursor: string | null = null;
+
+    while (hasNextVariantPage) {
+      const productIds: string = ids.join(',');
+      console.log(`"product_ids:${productIds}"`);
+      const variantResponse: any = await admin.graphql(`
+      #graphql
+      query getProductsVariants($after: String) {
+        productVariants(first: 5, query: "product_ids:6933517762632,6933517598792,6933517074504,6933514977352,6933515698248,6933517795400,6933515665480,6933515468872", after: $after) {
+          nodes {
+            title
+            inventoryQuantity
+            price
+            compareAtPrice
+          }
+          pageInfo {
+            hasNextPage
+            endCursor
+          }
+        }
+      }
+    `,
+        {
+          variables: {
+            after: afterVariantCursor,
+            // query: `product_ids:${productIds}`
+          }
+        }
+      );
+      const variantData: any = await variantResponse.json();
+      // console.log(variantData)
+      console.log("variants data: ", variantData.data.productVariants.nodes);
+      hasNextVariantPage = variantData.data.productVariants.pageInfo.hasNextPage;
+      afterVariantCursor = variantData.data.productVariants.pageInfo.endCursor;
+      console.log("Next variant page or not", hasNextVariantPage);
     }
+    ids = [];
+
     afterCursor = productsData.collection.products.pageInfo.endCursor;
     hasNextPage = productsData.collection.products.pageInfo.hasNextPage;
   }
-  
+
   return {
     collectionId: collectionId,
     products: products
   }
 };
 
-export const action = async ({ request}: ActionFunctionArgs) => {
+export const action = async ({request}: ActionFunctionArgs) => {
   console.log("Action Triggered.");
   const {admin} = await authenticate.admin(request);
 
-  if(!admin) {
+  if (!admin) {
     throw new Error('admin not authenticated!');
   }
 
@@ -96,30 +148,31 @@ export const action = async ({ request}: ActionFunctionArgs) => {
       }
     }
   `,
-  {
-    variables: {
-      id: collectionId,
-      moves: JSON.parse(moves),
+    {
+      variables: {
+        id: collectionId,
+        moves: JSON.parse(moves),
+      },
     },
-  },
+  ).catch(e => {
+    console.log(e.response)
+  })
 
-).catch(e=>{console.log(e.response)})
-
-const responseData: any = await response.json();
+  const responseData: any = await response.json();
 // console.log(responseData);
-console.log("responseData.data.collectionReorderProducts.job",responseData.data.collectionReorderProducts.job)
+  console.log("responseData.data.collectionReorderProducts.job", responseData.data.collectionReorderProducts.job)
 
-console.log("responseData.data.collectionReorderProducts.userErrors",responseData.data.collectionReorderProducts.userErrors)
+  console.log("responseData.data.collectionReorderProducts.userErrors", responseData.data.collectionReorderProducts.userErrors)
 
 
-return {JobId: responseData.data.collectionReorderProducts.userErrors}
+  return {JobId: responseData.data.collectionReorderProducts.userErrors}
 }
 
 export default function VMId() {
-  const { collectionId, products } = useLoaderData<typeof loader>();
+  const {collectionId, products} = useLoaderData<typeof loader>();
   const fetcher = useFetcher<typeof action>();
   const [reorderData, setReorderData] = useState(products);
-  console.log(products);
+  console.dir(products, {depth: null});
   // console.log('fetcher: ', fetcher);
   const handleReorder = () => {
     const moves = reorderData.map((product, index) => ({
@@ -131,10 +184,10 @@ export default function VMId() {
 
     fetcher.submit(
       {
-        id: collectionId, 
-        moves: movesValue, 
+        id: collectionId,
+        moves: movesValue,
       },
-      { method: "POST", encType: "application/json" }
+      {method: "POST", encType: "application/json"}
     );
   };
   return (
@@ -149,30 +202,31 @@ export default function VMId() {
               description={product.node.title}
               primaryAction={{
                 content: 'Learn more',
-                onAction: () => {},
+                onAction: () => {
+                },
               }}
             >
               <img
                 alt=""
                 width="200px"
                 height="200px"
-                style={{ objectFit: 'cover', objectPosition: 'center' }}
+                style={{objectFit: 'cover', objectPosition: 'center'}}
                 src="https://burst.shopifycdn.com/photos/business-woman-smiling-in-office.jpg?width=1850"
               />
             </MediaCard>
           ))}
         </Card>
         <fetcher.Form method='POST'>
-          <input type="hidden" name="id" value={collectionId} />
+          <input type="hidden" name="id" value={collectionId}/>
           <input
             type='hidden'
             name='moves'
             value={JSON.stringify([
-              {id: "gid://shopify/Product/7847532003402", newPosition: "0"},
-              {id: "gid://shopify/Product/7847531511882", newPosition: "1"}
+              {id: "gid://shopify/Product/6933515468872", newPosition: "0"},
+              {id: "gid://shopify/Product/6933515698248", newPosition: "1"}
             ])}
-          /> 
-          <div style={{ padding: "16px" }}>
+          />
+          <div style={{padding: "16px"}}>
             <button onClick={handleReorder}>Reorder Products</button>
           </div>
         </fetcher.Form>
